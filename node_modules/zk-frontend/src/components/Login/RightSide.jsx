9ba28a7fe8b0336d23139bc025f335ef06e8fb97
AB7zz <abhinavcv007@gmail.com>
2023-11-29 23:32:30 +0530

    register set

---
+++ node_modules/zk-frontend/src/components/Login/RightSide.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const RightSide = () => {
+  return (
+    <div className='glass-effect rounded-tr-[15px] rounded-br-[15px] md:rounded-tl-[0px] md:rounded-bl-[0px] rounded-tl-[15px] rounded-bl-[15px] h-full w-full py-10 px-10'>
+        <h1 className='text-center text-[#D01987] poppins font-medium text-2xl'>Login</h1>
+        <div className='my-10'>
+        <div className='mb-5'>
+            <label className='text-[#505050] poppins'>Email</label>
+            <input
+            type="text" 
+            name="" 
+            className='mt-3 bg-[#DEDEDE] rounded-[10px] pl-4 py-2 w-full focus:!outline-none text-[#A7A7A7] focus:!scale-105 transition duration-300 ease-in-out' id="" />
+        </div>
+        <div className='mb-5'>
+            <label className='text-[#505050] poppins'>Password</label>
+            <input
+            type="password" 
+            name="" 
+            className='mt-3 bg-[#DEDEDE] rounded-[10px] pl-4 py-2 w-full focus:!outline-none text-[#A7A7A7] focus:!scale-105 transition duration-300 ease-in-out' id="" />
+        </div>
+        <p className='text-black poppins'>Do not have an account? <Link to='/signup' className='text-[#D01987]'>Sign up</Link></p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+        <button
+        className='bg-[#75ACFF] text-[#EFEFEF] text-center px-10 py-2 rounded-[10px] poppins hover:bg-[#5C8BFF] transition duration-300 ease-in-out'
+        > 
+            Login
+        </button>
+        <span className='text-center text-[#505050] poppins text-2xl my-5'>OR</span>
+        <Link to='/signup'>
+            <button
+            className='bg-[#383838] text-[#EFEFEF] text-center px-10 py-2 rounded-[10px] poppins hover:bg-[#2A2A2A] transition duration-300 ease-in-out'
+            > 
+            Sign up
+            </button>
+        </Link>
+        </div>
+    </div>
+  )
+}
+
+export default RightSide
