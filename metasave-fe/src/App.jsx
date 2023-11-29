05f90aa0c765b43a3bdd64389324c8663ceab4dd
AB7zz <abhinavcv007@gmail.com>
2023-11-29 20:32:52 +0530

    responsive login page

--- metasave-fe/src/App.jsx
+++ metasave-fe/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
