cd6e2337ee1b2d81f65d0e80fdc950e1a6fa7c8b
Alosh Denny <97750960+aloshdenny@users.noreply.github.com>
2023-12-16 16:45:34 +0530

    added dependencies

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-# Calculate angles for both left and right sides
-            # for keypoint_index, keypoint_coords in enumerate(human.xy[0]):
-            #     # Draw a circle at the keypoint location
-            #     keypoint_x, keypoint_y = keypoint_coords.detach().cpu().numpy()
-            #     cv2.circle(annotated_frame, (int(keypoint_x), int(keypoint_y)), 5, (0, 0, 255), -1)
-
-            #     # Display the keypoint number next to the circle
-            #     cv2.putText(annotated_frame, str(keypoint_index + 1), (int(keypoint_x) + 10, int(keypoint_y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
