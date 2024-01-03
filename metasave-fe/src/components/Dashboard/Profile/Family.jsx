b98472240b5d2f79f6d8d4bab01c955a2e951ce7
AB7zz <abhinavcv007@gmail.com>
2024-01-03 16:47:24 +0530

    family completed

--- metasave-fe/src/components/Dashboard/Profile/Family.jsx
+++ metasave-fe/src/components/Dashboard/Profile/Family.jsx
@@ -3,46 +3,52 @@ import AddIcon from '@mui/icons-material/Add';
 import '../../../styles/Family.css'
 import { useMainContext } from '../../../context/MainContext';
 import { useSignupContext } from '../../../pages/Signup';
+import axios from 'axios'
 
-const NewCard = ({index, handleContactChange, contact}) => {
+const NewCard = ({index, handleContactChange, contacts, userDetails, setUserDetails, setContacts}) => {
+    const {serverUrl} = useMainContext()
+
     const [edit, setEdit] = React.useState(true)
     const [deleteThis, setDelete] = React.useState(false)
-    const handleEdit = () => {
-        setEdit(true)
-        console.log(i)
-    }
+
     const handleDelete = () => {
+        setContacts([{name: '', phoneNumber: ''}])
         setDelete(false)
-        console.log(i)
     }
-    const handleSave = () => {
+    const handleSave = async() => {
+        const newContact = contacts.slice(1);
+        const mergedContacts = [...userDetails.contacts, ...newContact];
+
+        const dummyData = userDetails
+        dummyData.contacts = mergedContacts
+
+        setUserDetails((prevDetails) => ({
+            ...prevDetails,
+            contacts: mergedContacts,
+        }));
+
+        const res = await axios.post(`${serverUrl}/register`, dummyData);
+        if(res.status == 200){
+            console.log('successful')
+        }
+        setContacts([{ name: '', phoneNumber: '' }])
         setEdit(false)
-        console.log(i)
     }
     return (
         <div key={index} className='family-card'>
-            {edit ?
-                <input onChange={(e) => handleContactChange(index, 'phoneNumber', e.target.value)} className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full'  />
-            :
-                <h3 className='text-[#3a3a3a] poppins text-center my-5 font-semibold'>{contact.phoneNumber}</h3>
-            }
-            {edit ?
-                <input onChange={(e) => handleContactChange(index, 'name', e.target.value)} className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full'  />
-            :
-                <h3 className='text-[#3a3a3a] poppins text-center my-5 font-semibold'>{contact.name}</h3>
-            }
+            <input onChange={(e) => handleContactChange(index, 'phoneNumber', e.target.value)} className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full'  />
+            
+            <input onChange={(e) => handleContactChange(index, 'name', e.target.value)} className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full'  />
+            
             <div className='mt-5'>
                 {
                 deleteThis ?
                 <>
-                    <button onClick={() => handleDelete()} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-bl-[15px] w-full'>Confirm</button>
-                    <button onClick={() => setDelete(false)} className='py-2 bg-[#CE576D] hover:bg-[#a92b42] text-white text-center rounded-br-[15px] w-full'>Cancel</button>
+                    <button onClick={() => setDelete(false)} className='py-2 bg-[#CE576D] hover:bg-[#a92b42] text-white text-center rounded-bl-[15px] w-full'>Cancel</button>
+                    <button onClick={handleDelete} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-br-[15px] w-full'>Confirm</button>
                 </>
                 :
-                edit ? 
-                    <button onClick={() => handleSave()} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-bl-[15px] w-full'>Save</button>
-                :
-                    <button onClick={() => handleEdit()} className='py-2 bg-[#3A3A3A] hover:bg-[#252525] text-white text-center rounded-bl-[15px] w-full'>Edit</button>
+                    <button onClick={handleSave} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-bl-[15px] w-full'>Save</button>
                 }
                 {!deleteThis && <button onClick={() => setDelete(true)} className='py-2 bg-[#CE576D] hover:bg-[#a92b42] text-white text-center rounded-br-[15px] w-full'>Delete</button>}
             </div>
@@ -50,30 +56,58 @@ const NewCard = ({index, handleContactChange, contact}) => {
     )
 }
 
-const Card = ({index, user}) => {
+const Card = ({index, user, userDetails, setUserDetails}) => {
+    const {serverUrl} = useMainContext()
+
     const [edit, setEdit] = React.useState(false)
     const [deleteThis, setDelete] = React.useState(false)
-    const handleEdit = (i) => {
+    const handleEdit = () => {
         setEdit(true)
-        console.log(i)
     }
-    const handleDelete = (i) => {
+
+    const handleChange = (field, value) => {
+        setUserDetails((prevDetails) => {
+            const updatedContacts = [...prevDetails.contacts];
+            updatedContacts[index][field] = value
+      
+            return {
+              ...prevDetails,
+              contacts: updatedContacts,
+            };
+        });
+    }
+    const handleDelete = async() => {
+        const dummyData = userDetails
+        dummyData.contacts = dummyData.contacts.filter((contact, i) => i !== index)
+        console.log(dummyData)
+        setUserDetails((prevDetails) => ({
+            ...prevDetails,
+            contacts: prevDetails.contacts.filter((contact, i) => i !== index),
+        }));
+
+        
+        const res = await axios.post(`${serverUrl}/register`, dummyData);
+        if(res.status == 200){
+            console.log('successful')
+        }
         setDelete(false)
-        console.log(i)
     }
-    const handleSave = (i) => {
+    const handleSave = async() => {
+        const res = await axios.post(`${serverUrl}/register`, userDetails);
+        if(res.status == 200){
+            console.log('successful')
+        }
         setEdit(false)
-        console.log(i)
     }
     return (
         <div key={index} className='family-card'>
             {edit ?
-                <input className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full' defaultValue={user.phoneNumber} />
+                <input onChange={(e) => handleChange('phoneNumber', e.target.value)} className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full' defaultValue={user.phoneNumber} />
             :
                 <h3 className='text-[#3a3a3a] poppins text-center my-5 font-semibold'>{user.phoneNumber}</h3>
             }
             {edit ?
-                <input className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full' defaultValue={user.name} />
+                <input onChange={(e) => handleChange('name', e.target.value)} className='text-[#3a3a3a] poppins text-center my-5 !font-bold bg-transparent focus:!outline-none border-b border-gray-400 border-b-2 pl-3 w-full' defaultValue={user.name} />
             :
                 <h3 className='text-[#3a3a3a] poppins text-center my-5 font-semibold'>{user.name}</h3>
             }
@@ -81,14 +115,14 @@ const Card = ({index, user}) => {
                 {
                 deleteThis ?
                 <>
-                    <button onClick={() => handleDelete(index)} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-bl-[15px] w-full'>Confirm</button>
-                    <button onClick={() => setDelete(false)} className='py-2 bg-[#CE576D] hover:bg-[#a92b42] text-white text-center rounded-br-[15px] w-full'>Cancel</button>
+                    <button onClick={() => setDelete(false)} className='py-2 bg-[#CE576D] hover:bg-[#a92b42] text-white text-center rounded-bl-[15px] w-full'>Cancel</button>
+                    <button onClick={handleDelete} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-br-[15px] w-full'>Confirm</button>
                 </>
                 :
                 edit ? 
-                    <button onClick={() => handleSave(index)} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-bl-[15px] w-full'>Save</button>
+                    <button onClick={handleSave} className='py-2 bg-green-500 hover:bg-green-800 text-white text-center rounded-bl-[15px] w-full'>Save</button>
                 :
-                    <button onClick={() => handleEdit(index)} className='py-2 bg-[#3A3A3A] hover:bg-[#252525] text-white text-center rounded-bl-[15px] w-full'>Edit</button>
+                    <button onClick={handleEdit} className='py-2 bg-[#3A3A3A] hover:bg-[#252525] text-white text-center rounded-bl-[15px] w-full'>Edit</button>
                 }
                 {!deleteThis && <button onClick={() => setDelete(true)} className='py-2 bg-[#CE576D] hover:bg-[#a92b42] text-white text-center rounded-br-[15px] w-full'>Delete</button>}
             </div>
@@ -97,17 +131,17 @@ const Card = ({index, user}) => {
 }
 
 const Family = () => {
-    const { userDetails } = useMainContext()
-    const { contacts, handleContactChange, addContact } = useSignupContext()
+    const { userDetails, setUserDetails } = useMainContext()
+    const { contacts, handleContactChange, addContact, setContacts } = useSignupContext()
   return (
     <div className='grid my-10'>
-        {userDetails && userDetails.contacts.map((user, index) => <Card index={index} user={user} /> )}
+        {userDetails && userDetails.contacts.map((user, index) => <Card setUserDetails={setUserDetails} userDetails={userDetails} index={index} user={user} /> )}
         {contacts && contacts.length > 1 && contacts.map((contact, index) => {
             if (index == 0){
                 return null
             }
             else {
-                return <NewCard contact={contact} handleContactChange={handleContactChange} index={index} />
+                return <NewCard userDetails={userDetails} setContacts={setContacts} setUserDetails={setUserDetails} contacts={contacts} handleContactChange={handleContactChange} index={index} />
             }
         })}
         <div onClick={addContact} className='family-card cursor-pointer justify-center items-center'>
