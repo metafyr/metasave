f67c74207119fa53f631dd8a52ee657c036df4b5
harshedabdulla <harshed2004@gmail.com>
2024-02-06 21:54:25 +0530

    Fall endpoint to send fall image to IPFS and retrieving the cid

--- packages/metasave-be/controllers/fall/index.js
+++ packages/metasave-be/controllers/fall/index.js
@@ -1,9 +1,16 @@
-const fall = async(req, res) => {
-    try{
-        
-    }catch(err){
+import { createHelia } from 'helia';
+import { unixfs } from '@helia/unixfs';
 
-    }
-}
+const fall = async (req, res) => {
+  try {
+    const helia = await createHelia();
+    const fs = unixfs(helia);
+    const file = req.file;
+    const result = await fs.add(file.path);
+    res.send({ cid: result });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
 
-export default fall
+export default fall;
