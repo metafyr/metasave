877a113859efc7db5744eade119ee718a5a2d401
AB7zz <abhinavcv007@gmail.com>
2024-02-08 13:48:29 +0530

    saving the data to IPFS and returning

--- packages/metasave-be/controllers/fall/index.js
+++ packages/metasave-be/controllers/fall/index.js
@@ -1,16 +1,31 @@
-import { createHelia } from 'helia';
-import { unixfs } from '@helia/unixfs';
+import { createHelia } from 'helia'
+import { unixfs } from '@helia/unixfs'
+import multer from 'multer'
+import { dagJson } from '@helia/dag-json'
+
+const helia = await createHelia()
+const fs = unixfs(helia)
+const d = dagJson(helia)
 
 const fall = async (req, res) => {
   try {
-    const helia = await createHelia();
-    const fs = unixfs(helia);
-    const file = req.file;
-    const result = await fs.add(file.path);
-    res.send({ cid: result });
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
-};
+    console.log(req.body)
+    const predictionData = JSON.parse(req.body.prediction_data)
+    const privKey = req.body.PRIV_KEY
+    const username = req.body.username
 
-export default fall;
+    // const imgIPFSid = await fs.add(`uploads/${username}/image.jpg`)
+    
+    const AddObject = await d.add(predictionData)
+    const data2 = { link: AddObject }
+    const AddObject2 = await d.add(data2)
+    const retrievedObject = await d.get(AddObject2)
+    const dataIPFSid = retrievedObject.link.toString()
+    console.log(dataIPFSid)
+    res.send({ dataIPFSid })
+  } catch (err) {
+    res.status(500).send({ error: err.message })
+  }
+}
+
+export default fall
--- packages/metasave-be/routes/fall.js
+++ packages/metasave-be/routes/fall.js
@@ -1,8 +1,28 @@
 import express from 'express'
 import fall from '../controllers/fall/index.js'
+import multer from 'multer'
+import path, { dirname } from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      const privKeyFolder = path.join(__dirname, '../uploads', req.body.username);
+      fs.mkdirSync(privKeyFolder, { recursive: true });
+      cb(null, privKeyFolder);
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'image.jpg');
+    }
+});
+  
+const upload = multer({ storage: storage });
 
 const router = express.Router()
 
-router.post('/fall', fall)
+router.post('/fall', upload.single('file'), fall)
 
 export default router
