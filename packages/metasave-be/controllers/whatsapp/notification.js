ed9f1517f212c7f8d5052783f051490a2f320c9d
AB7zz <abhinavcv007@gmail.com>
2024-02-13 00:24:24 +0530

    whatsapp helper and comments on what to do next

--- packages/metasave-be/controllers/whatsapp/notification.js
+++ packages/metasave-be/controllers/whatsapp/notification.js
@@ -1,68 +1,18 @@
-import axios from 'axios'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const GRAPH_API_TOKEN = process.env.GRAPH_API_TOKEN
+import sendMessage from "../../helpers/whatsapp/sendMessage"
 
 const whatsappNotification = async(req, res) => {
     try {
-        var reqdata = req.body
-        var options = {
-            method: 'POST',
-            url: 'https://graph.facebook.com/v17.0/180851955102577/messages',
-            headers: {
-                Authorization: `Bearer ${GRAPH_API_TOKEN}`,
-                'Content-Type': 'application/json'
-            },
-            data: {
-                messaging_product: 'whatsapp',
-                to: '919188020170',
-                type: 'template',
-                template: {
-                name: 'alert', 
-                language: {code: 'en'},
-                components: [
-                    {
-                    "type": "header",
-                    "parameters": [
-                        {
-                        "type": "image",
-                        "image": {
-                            "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfntmfcua0MmAbzlL8XeTZLSg5HGb0SDCzOQ&usqp=CAU",
-                        }
-                        }
-                    ]
-                    },
-                    {
-                    "type": "body",
-                    "parameters": [
-                        {
-                        "type": "text",
-                        "text": req.body.name
-                        },
-                        {
-                        "type": "text",
-                        "text": "15:44 AM"
-                        },
-                        {
-                        "type": "text",
-                        "text": "2023-12-20"
-                        }
-                    ]
-                    }
-                ]
-                }
+        const CFAddress = req.body.CFAddress
+        let phoneNumbers = []
+        // fetch file name from IPFS
+        //fetch userDetails from Blockchain
+        //loop through phone numbers and send message
+        for (let i = 0; i < phoneNumbers.length; i++) {
+            const res = sendMessage(phoneNumbers[i])
+            if(!res){
+                console.log('Error sending message to', phoneNumbers[i])
             }
         }
-            
-        axios.request(options).then(function (response) {
-            console.log(response.data);
-            res.send(response.data);
-        }).catch(function (error) {
-            console.error(error);
-            res.send(error);
-        });
     } catch (error) {
         console.log(error)
     }
