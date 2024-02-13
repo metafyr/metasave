c7a2e6378f071625e530338772a8d138f4f313c4
AB7zz <abhinavcv007@gmail.com>
2024-02-13 19:35:58 +0530

    insert and fetch user details route

--- packages/metasave-be/controllers/user/fetchDetails.js
+++ packages/metasave-be/controllers/user/fetchDetails.js
@@ -1,6 +1,12 @@
+import { j } from './insertDetails.js'
+import CID from 'cids'
+
 const fetchDetails = async(req, res) => {
     try {
-        
+        const cid = new CID('bagaaieragt4ev55rjvvs4ere6td357exmdzlwommiz254s5veibruc2nrg3q')
+        const data = await j.get(cid)
+        console.log(data)
+        res.json(data)
     } catch (error) {
         console.log(error)
     }
--- packages/metasave-be/controllers/user/insertDetails.js
+++ packages/metasave-be/controllers/user/insertDetails.js
@@ -2,15 +2,15 @@ import { createHelia } from 'helia'
 import { json } from '@helia/json'
 
 const helia = await createHelia()
-const j = json(helia)
+export const j = json(helia)
 
 const insertDetails = async(req, res) => {
     try{
-        console.log(req.body.data)
         const data = req.body.data 
-        const myImmutableAddress = await j.add({ hello: 'world' })
-
+        const myImmutableAddress = await j.add(data)
+        console.log(myImmutableAddress)
         console.log(await j.get(myImmutableAddress))
+        res.json({CID: myImmutableAddress.toString()})
     }catch(err){
         console.log(err)
     }
