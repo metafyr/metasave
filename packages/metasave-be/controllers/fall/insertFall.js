bf9df6db3a2dd3ddb409ac84568d25a2e11091eb
AB7zz <abhinavcv007@gmail.com>
2024-02-22 00:22:34 +0530

    insert image doesnt work

--- packages/metasave-be/controllers/fall/insertFall.js
+++ packages/metasave-be/controllers/fall/insertFall.js
@@ -3,7 +3,13 @@ import { promises as fsPromises } from 'fs';
 import { userOperation } from '../../helpers/userOperation.js';
 import { abi } from '../../abi/index.js';
 import { FormData } from "formdata-node"
+import dotenv from 'dotenv'
+import axios from 'axios'
+import fs from 'fs'
 
+dotenv.config()
+
+const PINATA_API_KEY = process.env.PINATA_API_KEY
 
 const insertFall = async (req, res) => {
   try {
@@ -14,8 +20,12 @@ const insertFall = async (req, res) => {
     const PRIV_KEY = req.body.PRIV_KEY
 
     // IPFS part
-    console.log(req.body)
     const predictionData = JSON.parse(req.body.prediction_data)
+    // const predictionData = {
+    //   fall: true,
+    //   probability: 0.8,
+    //   name: "abhinav"
+    // }
     const pinataMetadata = JSON.stringify(predictionData)
     const jsonFile = new Blob([pinataMetadata], { type: 'application/json' })
     formData.set('file', jsonFile)
@@ -36,20 +46,22 @@ const insertFall = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-    const privKey = req.body.PRIV_KEY
+
     const username = req.body.username
 
     const imagePath = `uploads/${username}/image.jpg`;
-    const imageBuffer = await fsPromises.readFile(imagePath)
-    const imageBlob = new Blob([imageBuffer], { type: 'image/jpeg' });
-    formData.set('file', imageBlob);
-    formData.set('pinataMetadata', '{"name": "fall img"}')
-    formData.set('pinataOptions', '{"cidVersion": 0}')
+
+    const file = fs.createReadStream(imagePath)
+    formData.set('file', file);
+    const pinataMetadata2 = JSON.stringify({
+      name: "File name",
+    });
+    formData.append("pinataMetadata", pinataMetadata2);
+
+    formData.append("pinataOptions", pinataOptions);
     try {
       const response = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
-        maxBodyLength: "Infinity",
         headers: {
-            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
             'Authorization': `Bearer ${PINATA_API_KEY}`
         }
       });
@@ -65,7 +77,7 @@ const insertFall = async (req, res) => {
     const txHash = await userOperation(abi.MetaSave, functionName, args, PRIV_KEY);
     res.send({ imgIPFSid, dataIPFSid, txHash })
   } catch (err) {
-    res.status(500).send({ error: err.message })
+    // res.status(500).send({ error: err.message })
   }
 }
 
