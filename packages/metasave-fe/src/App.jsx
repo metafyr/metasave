df71ef0ba1a6089c1014e3db4b6f764b413b5596
harshedabdulla <harshed2004@gmail.com>
2024-02-24 21:52:28 +0530

    redirecting directly to dashboard instead of alreadyloggedin

--- packages/metasave-fe/src/App.jsx
+++ packages/metasave-fe/src/App.jsx
@@ -1,31 +1,44 @@
 import './styles/App.css'
 import './init.js'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom' // Import Navigate for redirection
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Signup from './pages/Signup'
 import React from 'react'
 import { useAuthContext } from './context/AuthContext.jsx'
 import PleaseLogin from './pages/PleaseLogin.jsx'
-import AlreadyLoggedIn from './pages/AlreadyLoggedIn.jsx'
 
 function App() {
-  const {loggedIn, web3auth, initWeb3Auth} = useAuthContext()
+  const { loggedIn, web3auth, initWeb3Auth } = useAuthContext()
   React.useEffect(() => {
     initWeb3Auth()
   }, [])
   return (
     <>
-      {web3auth && <BrowserRouter>
-        <Routes>
-          <Route path="/" element={!loggedIn ? <Login /> : <AlreadyLoggedIn />} />
-          <Route path="/login" element={!loggedIn ? <Login /> : <AlreadyLoggedIn />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/register" element={<Signup />} />
-          <Route path="/dashboard" element={loggedIn ? <Dashboard /> : <PleaseLogin />} />
-          <Route path="/dashboard/*" element={loggedIn ? <Dashboard /> : <PleaseLogin />} />
-        </Routes>
-      </BrowserRouter>}
+      {web3auth && (
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={loggedIn ? <Navigate to="/dashboard" /> : <Login />}
+            />
+            <Route
+              path="/login"
+              element={loggedIn ? <Navigate to="/dashboard" /> : <Login />}
+            />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/register" element={<Signup />} />
+            <Route
+              path="/dashboard"
+              element={loggedIn ? <Dashboard /> : <PleaseLogin />}
+            />
+            <Route
+              path="/dashboard/*"
+              element={loggedIn ? <Dashboard /> : <PleaseLogin />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
   )
 }
