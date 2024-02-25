1ecd4376095cc1131fae525bc6cdf9e61a4e2a8b
harshedabdulla <harshed2004@gmail.com>
2024-02-25 22:11:42 +0530

    inserting data and image in ipfs (tested)

--- packages/metasave-be/controllers/fall/insertFall.js
+++ packages/metasave-be/controllers/fall/insertFall.js
@@ -1,11 +1,9 @@
-
-import { promises as fsPromises } from 'fs';
-import { userOperation } from '../../helpers/userOperation.js';
-import { abi } from '../../abi/index.js';
-import { FormData } from "formdata-node"
-import dotenv from 'dotenv'
-import axios from 'axios'
 import fs from 'fs'
+import axios from 'axios'
+import FormData from 'form-data'
+import dotenv from 'dotenv'
+import { userOperation } from '../../helpers/userOperation.js'
+import { abi } from '../../abi/index.js'
 
 dotenv.config()
 
@@ -13,71 +11,65 @@ const PINATA_API_KEY = process.env.PINATA_API_KEY
 
 const insertFall = async (req, res) => {
   try {
-    let dataIPFSid, imgIPFSid = ""
+    let imgIPFSid = ''
+    let dataIPFSid = ''
+    const imagePath = `./uploads/${req.body.username}/image.jpg`
 
-    const formData = new FormData()
+    // Upload image to IPFS
+    if (fs.existsSync(imagePath)) {
+      const formDataImage = new FormData()
+      formDataImage.append('file', fs.createReadStream(imagePath))
 
-    const PRIV_KEY = req.body.PRIV_KEY
-
-    // IPFS part
-    const predictionData = JSON.parse(req.body.prediction_data)
-    // const predictionData = {
-    //   fall: true,
-    //   probability: 0.8,
-    //   name: "abhinav"
-    // }
-    const pinataMetadata = JSON.stringify(predictionData)
-    const jsonFile = new Blob([pinataMetadata], { type: 'application/json' })
-    formData.set('file', jsonFile)
-    formData.set('pinataMetadata', pinataMetadata);
-    const pinataOptions = JSON.stringify({
-      cidVersion: 0,
-    })
-    formData.set('pinataOptions', pinataOptions);
-    try{
-      const response = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
-        maxBodyLength: "Infinity",
-        headers: {
-            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-            'Authorization': `Bearer ${PINATA_API_KEY}`
-        }
-      });
-      dataIPFSid = response.data.IpfsHash
-    } catch (error) {
-        console.log(error);
+      imgIPFSid = await uploadToIPFS(formDataImage, 'file')
+      console.log('Image IPFS ID:', imgIPFSid)
+    } else {
+      console.log('Image file does not exist:', imagePath)
     }
 
-    const username = req.body.username
+    dataIPFSid = await uploadToIPFS(req.body.prediction_data, 'json')
+    console.log('Data IPFS ID:', dataIPFSid)
 
-    const imagePath = `uploads/${username}/image.jpg`;
-
-    const file = fs.createReadStream(imagePath)
-    formData.set('file', file);
-    const pinataMetadata2 = JSON.stringify({
-      name: "File name",
-    });
-    formData.append("pinataMetadata", pinataMetadata2);
-
-    formData.append("pinataOptions", pinataOptions);
-    // try {
-    //   const response = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
-    //     headers: {
-    //         'Authorization': `Bearer ${PINATA_API_KEY}`
-    //     }
-    //   });
-    //   imgIPFSid = response.data.IpfsHash
-    // } catch (error) {
-    //   console.log(error);
-    // }
-    
-    // Blockchain part 
-    const functionName = "setFallData";
-    args = [CFAddress, imgIPFSid, dataIPFSid];
-    
-    const txHash = await userOperation(abi.MetaSave, functionName, args, PRIV_KEY);
-    res.send({ imgIPFSid, dataIPFSid, txHash })
+    res.send({
+      imgIPFSid,
+      dataIPFSid,
+      txHash: 'Blockchain interaction not implemented',
+    })
   } catch (err) {
+    console.error('Server error:', err)
     res.status(500).send({ error: err.message })
+  }
+}
+
+async function uploadToIPFS(data, type) {
+  try {
+    if (type === 'json') {
+      const response = await axios.post(
+        'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${PINATA_API_KEY}`,
+          },
+        }
+      )
+      return response.data.IpfsHash
+    } else if (type === 'file') {
+      const response = await axios.post(
+        'https://api.pinata.cloud/pinning/pinFileToIPFS',
+        data,
+        {
+          headers: {
+            ...data.getHeaders(),
+            Authorization: `Bearer ${PINATA_API_KEY}`,
+          },
+          maxBodyLength: 'Infinity',
+        }
+      )
+      return response.data.IpfsHash
+    }
+  } catch (error) {
+    console.error('Error uploading to IPFS:', error)
+    throw new Error('Failed to upload to IPFS.')
   }
 }
 
--- packages/metasave-be/routes/fall.js
+++ packages/metasave-be/routes/fall.js
@@ -4,23 +4,24 @@ import fetchFall from '../controllers/fall/fetchFall.js'
 import multer from 'multer'
 import path, { dirname } from 'path'
 import fs from 'fs'
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      const privKeyFolder = path.join(__dirname, '../uploads', req.body.username);
-      fs.mkdirSync(privKeyFolder, { recursive: true });
-      cb(null, privKeyFolder);
-    },
-    filename: function (req, file, cb) {
-      cb(null, 'image.jpg');
-    }
-});
-  
-const upload = multer({ storage: storage });
+  destination: function (req, file, cb) {
+    const username = req.body.username
+    const uploadsDir = path.join(__dirname, `../uploads/${username}`)
+    fs.mkdirSync(uploadsDir, { recursive: true })
+    cb(null, uploadsDir)
+  },
+  filename: function (req, file, cb) {
+    cb(null, 'image.jpg')
+  },
+})
+
+const upload = multer({ storage: storage })
 
 const router = express.Router()
 
