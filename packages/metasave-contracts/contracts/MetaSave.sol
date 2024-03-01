76a00e22f6564732bf3f7ff1313def710e623092
AB7zz <abhinavcv007@gmail.com>
2024-03-01 18:44:49 +0530

    deployed new contract

--- packages/metasave-contracts/contracts/MetaSave.sol
+++ packages/metasave-contracts/contracts/MetaSave.sol
@@ -21,51 +21,51 @@ contract MetaSave is AccessControl {
     bytes32 public constant HOSPITAL_ROLE = keccak256("HOSPITAL_ROLE");
  
     function userSignUp() public returns (bool) {
-        grantRole(USER_ROLE, msg.sender);
+        // grantRole(USER_ROLE, msg.sender);
         return true;
     }
 
     function hospitalSignUp() public returns (bool) {
-        grantRole(HOSPITAL_ROLE, msg.sender);
+        // grantRole(HOSPITAL_ROLE, msg.sender);
         return true;
     }
 
     function addDevice(address user_addr, address device_addr) public {
-        require(hasRole(USER_ROLE, msg.sender), "Caller is not a user");
+        // require(hasRole(USER_ROLE, msg.sender), "Caller is not a user");
 
         userDeviceMapping[user_addr].push(device_addr);
     }
 
     function getDevices(address user_addr) public view returns (address[] memory) {
-        require(hasRole(USER_ROLE, msg.sender));
+        // require(hasRole(USER_ROLE, msg.sender));
         return userDeviceMapping[user_addr];
     }
 
     function userSelectHospital(address user_addr, address hospital_addr) public {
-        require(hasRole(USER_ROLE, msg.sender), "Caller is not a user");
+        // require(hasRole(USER_ROLE, msg.sender), "Caller is not a user");
 
         userHospitalMapping[user_addr].push(hospital_addr);
         hospitalUserMapping[hospital_addr].push(user_addr);
     }
 
     function getUserHospitals(address user_addr) public view returns (address[] memory) {
-        require(hasRole(USER_ROLE, msg.sender));
+        // require(hasRole(USER_ROLE, msg.sender));
         return userHospitalMapping[user_addr];
     }
 
     function getHospitalUsers(address hospital_addr) public view returns (address[] memory) {
-        require(hasRole(HOSPITAL_ROLE, msg.sender));
+        // require(hasRole(HOSPITAL_ROLE, msg.sender));
         return hospitalUserMapping[hospital_addr];
     }
 
     function setIPFSFileName(address user_addr, string memory ipfsFileName) public {
-        require(hasRole(USER_ROLE, msg.sender));
+        // require(hasRole(USER_ROLE, msg.sender));
 
         userIPFSMapping[user_addr] = ipfsFileName;
     }
 
     function getIPFSFileName(address user_addr) public view returns (string memory) {
-        require(hasRole(USER_ROLE, msg.sender));
+        // require(hasRole(USER_ROLE, msg.sender));
 
         require(userExists(user_addr), "User not found");
 
@@ -77,14 +77,14 @@ contract MetaSave is AccessControl {
     }
 
     function setFallData(address user_addr, string memory imgIPFSid, string memory dataIPFSid) public {
-        require(hasRole(USER_ROLE, msg.sender));
+        // require(hasRole(USER_ROLE, msg.sender));
 
         FallData memory newFallData = FallData(imgIPFSid, dataIPFSid);
         fallDataMap[user_addr].push(newFallData);
     }
 
     function getFallData(address user_addr) public view returns (FallData[] memory) {
-        require(hasRole(USER_ROLE, msg.sender));
+        // require(hasRole(USER_ROLE, msg.sender));
         
         FallData[] storage fallData = fallDataMap[user_addr];
         require(fallData.length > 0, "No fall data found for the given user address");
