579b9cd0ecb7905cd2a1d16105335b48547f6556
Alosh Denny <97750960+aloshdenny@users.noreply.github.com>
2024-03-11 22:57:59 +0530

    Update main.py

--- model/main.py
+++ model/main.py
@@ -5,12 +5,14 @@ from torchvision import transforms
 import numpy as np
 from yoloutils.datasets import letterbox
 from yoloutils.general import non_max_suppression
-from yoloutils.plots import output_to_target, plot_skeleton_kpts
+from yoloutils.plots import output_to_target
 from datetime import datetime
 import requests
 import json
 from io import BytesIO
 from dotenv import dotenv_values
+import threading
+import queue
 
 env_vars = dotenv_values()
 
@@ -40,6 +42,41 @@ frame_count = 0
 fallen = False
 sent = False
 
+q = queue.Queue()
+
+def post_request():
+    while True:
+        item = q.get()
+        if item is None:
+            break
+
+        prediction_data, buffer = item
+        prediction_data_json = json.dumps(prediction_data)
+
+        in_memory_file = BytesIO(buffer)
+        files = {'file': ('current_frame.jpg', in_memory_file, 'image/jpeg')}
+        data = {
+            'prediction_data': prediction_data_json,
+            'username': 'ab7zz',
+            'PRIV_KEY': PRIV_KEY
+        }
+
+        response = requests.post(url, files=files, data=data)
+
+        if response.status_code == 200:
+            print("Request sent successfully.")
+            res = json.loads(response.text)
+            print(f"Data IPFS ID: {res['dataIPFSid']}")
+            print(f"Image IPFS ID: {res['imgIPFSid']}")
+            print(f"Transaction Hash: {res['txHash']}")
+        else:
+            print("Error:", response.status_code)
+
+        q.task_done()
+
+request_thread = threading.Thread(target=post_request)
+request_thread.start()
+
 while(cap.isOpened):
     
     frame_count += 1  
@@ -66,10 +103,6 @@ while(cap.isOpened):
         
         im0 = cv2.cvtColor(im0, cv2.COLOR_RGB2BGR)
         for idx in range(output.shape[0]):
-            # plot_skeleton_kpts(im0, output[idx, 7:].T, 3)
-            # xmin, ymin = (output[idx, 2]-output[idx, 4]/2), (output[idx, 3]-output[idx, 5]/2)
-            # xmax, ymax = (output[idx, 2]+output[idx, 4]/2), (output[idx, 3]+output[idx, 5]/2)
-
             left_shoulder_y= output[idx][23]
             left_shoulder_x= output[idx][22]
             right_shoulder_y= output[idx][26]
@@ -96,33 +129,13 @@ while(cap.isOpened):
                   'date': date,
                   'status': 'fallen'
                 }
-                prediction_data_json = json.dumps(prediction_data)
 
-                in_memory_file = BytesIO(buffer)
-                
-                files = {'file': ('./current_frame.jpg', in_memory_file, 'image/jpeg')}
-                data = {
-                    'prediction_data': prediction_data_json,
-                    'username': 'ab7zz',
-                    'PRIV_KEY': PRIV_KEY
-                }
-
-                response = requests.post(url, files=files, data=data)
-
-                if response.status_code == 200:
-                    print("Request sent successfully.")
-                    res = json.loads(response.text)
-                    print(f"Data IPFS ID: {res['dataIPFSid']}")
-                    print(f"Image IPFS ID: {res['imgIPFSid']}")
-                    print(f"Transaction Hash: {res['txHash']}")
-                else:
-                    print("Error:", response.status_code)
+                q.put((prediction_data, buffer))
 
                 sent = True
             else:
                sent = False
                fallen = False
-        im0 = cv2.resize(im0, (im0.shape[1] // 2, im0.shape[0] // 2))
         cv2.imshow('image', im0)
         cv2.waitKey(1)
 
@@ -130,6 +143,9 @@ while(cap.isOpened):
             break
     else:
         break
+
+q.put(None)
+request_thread.join()
 
 cap.release()
 cv2.destroyAllWindows()
