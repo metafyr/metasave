5eb96cd89363c04324d4fa63eb59ad6d302d8552
Alosh Denny <97750960+aloshdenny@users.noreply.github.com>
2024-03-13 22:54:15 +0530

    Update nano_ble.ino

--- model/nano_ble.ino
+++ model/nano_ble.ino
@@ -49,6 +49,7 @@ void loop() {
     if ((abs(x)>=3.99 and abs(y)>=3.99) || (abs(y)>=3.99 and abs(z)>=3.99) || (abs(z)>=3.99 and abs(x) >=3.99)) {
       Characteristic.writeValue(1);
     } else {
+      Characteristic.writeValue(0);
     }
         
   }
